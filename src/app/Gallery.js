690b1396ae61415e49f4763afa08a51223ce8791
Abencar <76166064+abencar@users.noreply.github.com>
2024-10-29 21:43:59 +0100

    Hecho ejercicio 1

--- src/app/Gallery.js
+++ src/app/Gallery.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { getImageUrl } from './utils.js';
 import Perfil from './Perfiles.js';
 
@@ -5,21 +6,24 @@ const Persona1 = {
   nombre: "Katsuko Saruhashi",
   profesion: "geoquímica",
   premios: 2,
-  descubrimiento: "un método para medir el dióxido de carbono en el agua de mar",
+  descripcion:"Premio Nobel de Física, Premio Nobel de Química, Medalla Davy, Medalla Matteucci",
+  descubrimientos: "un método para medir el dióxido de carbono en el agua de mar",
   src: getImageUrl('szV5sdG'),
   alt: "Maria Skłodowska-Curie",
   width: 70,
   height: 70
 };
+
 const Persona2 = {
   nombre: "Maria Skłodowska-Curie",
   profesion: "física y química",
   premios: 4,
-  descubrimiento: "polonio (elemento químico)",
+  descripcion: "Premio Miyake de geoquímica, Premio Tanaka",
+  descubrimientos: "polonio (elemento químico)",
   src :getImageUrl('YfeOqp2'),
   alt : "Katsuko Saruhashi",
   width: 70,
-  height :70,
+  height: 70
 };
 
 
@@ -27,31 +31,8 @@ export default function Gallery() {
   return (
     <div>
       <h1>Científicos Notables</h1>
-      <Perfil />
-      <section className="profile">
-        <h2>Katsuko Saruhashi</h2>
-        <img
-          className="avatar"
-          src={getImageUrl('YfeOqp2')}
-          alt="Katsuko Saruhashi"
-          width={70}
-          height={70}
-        />
-        <ul>
-          <li>
-            <b>Profesión: </b> 
-            geoquímica
-          </li>
-          <li>
-            <b>Premios: 2 </b> 
-            (Premio Miyake de geoquímica, Premio Tanaka)
-          </li>
-          <li>
-            <b>Descubrió: </b>
-            un método para medir el dióxido de carbono en el agua de mar
-          </li>
-        </ul>
-      </section>
+      <Perfil {...Persona1}/>
+      <Perfil {...Persona2}/>
     </div>
   );
 }
--- src/app/Perfiles.js
+++ src/app/Perfiles.js
@@ -1,27 +1,30 @@
+import React from 'react';
 
-export default function Perfil() {
-  return(<section className="perfil">
-    <h2>Maria Skłodowska-Curie</h2>
-    <img
-      className="avatar"
-      src={getImageUrl('szV5sdG')}
-      alt="Maria Skłodowska-Curie"
-      width={70}
-      height={70}
-    />
-    <ul>
-      <li>
-        <b>Profesión: </b> 
-        física y química
-      </li>
-      <li>
-        <b>Premios: 4 </b> 
-        (Premio Nobel de Física, Premio Nobel de Química, Medalla Davy, Medalla Matteucci)
-      </li>
-      <li>
-        <b>Descubrió: </b>
-        polonio (elemento químico)
-      </li>
-    </ul>
-  </section>)
+export default function Perfil({ nombre, profesion, premios, descripcion, descubrimientos, src, alt, width, height }) {
+  return (
+    <section className="perfil">
+      <h2>{nombre}</h2>
+      <img
+        className="avatar"
+        src={src}
+        alt={alt}
+        width={width}
+        height={height}
+      />
+      <ul>
+        <li>
+          <b>Profesión: </b> 
+          {profesion}
+        </li>
+        <li>
+          <b>Premios: </b> 
+          {premios} ({descripcion})
+        </li>
+        <li>
+          <b>Descubrió: </b>
+          {descubrimientos}
+        </li>
+      </ul>
+    </section>
+  );
 }
